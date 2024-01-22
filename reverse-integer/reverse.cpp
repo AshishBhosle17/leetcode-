ec7ddd4b720f51adbf661f37e9948a7985aac465
Ashish Bhosle <140106733+AshishBhosle17@users.noreply.github.com>
2024-01-22 10:33:20 -0800

    Add files via upload

---
+++ reverse-integer/reverse.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverse(int x) {
+       
+        long r=0;      // decleare r 
+        while(x){
+         r=r*10+x%10; // find remainder and add its to r
+         x=x/10;     // Update the value of x
+        }
+        if(r>int_max || r<int_min) return 0; // check range if r is outside the range then return 0  
+        return int(r);
+        
+    }
+};
