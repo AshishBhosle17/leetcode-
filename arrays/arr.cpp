1dd0f8fce49e08487bd172273b45c0841048872c
Ashish Bhosle <140106733+AshishBhosle17@users.noreply.github.com>
2024-02-05 09:41:52 -0800

    Add files via upload

--- arrays/arr.cpp
+++ arrays/arr.cpp
@@ -139,3 +139,20 @@ public:
         
     }
 };
+
+// Buy and sell STOCKS
+
+int bestTimeToBuyAndSellStock(vector<int>&prices) {
+    // Write your code here.
+    int maxPro = 0;
+    int n = prices.size();
+    int minPrice = INT_MAX;
+
+    for (int i = 0; i < prices.size(); i++) {
+        minPrice = min(minPrice, prices[i]);
+        maxPro = max(maxPro, prices[i] - minPrice);
+    }
+    
+    return maxPro;
+
+}
